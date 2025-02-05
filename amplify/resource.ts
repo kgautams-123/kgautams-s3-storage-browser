ea008326ee33464879bd40e3c2211bbda23108e0
kgautams-123 <gautamsingh07@gmail.com>
2025-02-05 17:09:31 +0530

    Add files via upload

---
+++ amplify/resource.ts
@@ -0,0 +1,11 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+  name: 's3-browser-demo',
+  access: (allow) => ({
+    'aws/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read', 'write', 'delete'])
+    ]
+  })
+});
